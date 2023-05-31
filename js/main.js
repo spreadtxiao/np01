378d4e05265140bd81aa915042aac72e6f58e7d8
Spreadt Xiao <spreadxiao@gmail.com>
2023-05-31 14:28:17 -0300

    001

---
+++ js/main.js
@@ -0,0 +1,7 @@
+document.querySelector(".menu-btn").addEventListener("click", () =>{
+    document.querySelector(".nav-menu").classList.toggle("show")
+});
+ScrollReveal().reveal(".showcase");
+ScrollReveal().reveal(".news-tarj", {delay: 500});
+ScrollReveal().reveal(".tarjeta-banner-one", {delay: 500});
+ScrollReveal().reveal(".tarjeta-banner-two", {delay: 500});
